f500e3ec5e8e1ce11d693ce4e58cdd0e7171b149
Eric Wallace <eswallace@comcast.net>
2019-06-27 14:06:44 -0700

    upd

---
+++ demo/src/components/Hotflip.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+import{postprocessHotflip} from './Attack'
+import {  
+  AccordionItem,
+  AccordionItemTitle,
+  AccordionItemBody,
+  } from 'react-accessible-accordion';
+
+export default class HotflipItem extends React.Component {
+  constructor(props) {
+    super(props)      
+  }
+  
+  render() {
+    const { attackDataObject2, attackModelObject2, requestDataObject2 } = this.props     
+    
+    var attack_visual2 = '';    
+    var attack_visual2_og = '';  
+    if (attackDataObject2 === undefined) {
+      attack_visual2 = " "
+    }
+    else{    
+      var [first,second] = postprocessHotflip(attackDataObject2["original"],attackDataObject2["final"][0])
+      attack_visual2 = second
+      attack_visual2_og = first        
+    }
+    
+  return (<div> <AccordionItem expanded={true}>
+          <AccordionItemTitle>
+            HotFlip Attack
+            <div className="accordion__arrow" role="presentation"/>
+          </AccordionItemTitle>
+          <AccordionItemBody>            
+            <p> <a href="https://arxiv.org/abs/1712.06751" target="_blank">HotFlip</a> flips words in the Hypothesis to change the model's prediction. We iteratively flip the word in the Hypothesis with the highest gradient until the prediction changes.</p>                                
+            {attack_visual2 != " " ? <p><strong>Original Input:</strong> {attack_visual2_og}</p> : <p style={{color: "#7c7c7c"}}>Press "flip words" to run HotFlip.</p>}    
+            {attack_visual2 != " " ? <p><strong>Flipped Input:</strong> {attack_visual2}</p> : <p></p>}                      
+                <button
+                  type="button"
+                  className="btn"
+                  style={{margin: "30px 0px"}}
+                  onClick={ () => attackModelObject2(requestDataObject2) }>Flip Words
+                </button>
+
+          </AccordionItemBody>
+        </AccordionItem></div>
+    )
+  }
+}
---
+++ demo/src/components/InputReduction.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+import{postprocessInputReduction} from './Attack'
+import {  
+  AccordionItem,
+  AccordionItemTitle,
+  AccordionItemBody,
+  } from 'react-accessible-accordion';
+
+export default class HotflipItem extends React.Component {
+  constructor(props) {
+    super(props)      
+  }
+  
+  render() {
+    const { attackDataObject, attackModelObject, requestDataObject } = this.props     
+
+    var attack_visual = '';    
+    var attack_visual_og = '';
+    if (attackDataObject === undefined) {
+      attack_visual = " "
+    }
+    else{    
+      var [first,second] = postprocessInputReduction(attackDataObject["original"],attackDataObject["final"][0])    
+      attack_visual = second
+      attack_visual_og = first
+    }
+
+  return (<div><AccordionItem expanded={true}>               
+      <AccordionItemTitle>
+            Input Reduction
+            <div className="accordion__arrow" role="presentation"/>
+          </AccordionItemTitle>
+          <AccordionItemBody>                  
+     <p> <a href="https://arxiv.org/abs/1804.07781" target="_blank">Input Reduction</a> removes as many words from the input as possible without changing the model's prediction.</p>
+      {attack_visual != " " ? <p><strong>Original Input:</strong> {attack_visual_og}</p> : <p style={{color: "#7c7c7c"}}>Press "reduce input" to run input reduction.</p>}    
+            {attack_visual != " " ? <p><strong>Reduced Input:</strong> {attack_visual}</p> : <p></p>}          
+        <button
+          type="button"
+          className="btn"
+          style={{margin: "30px 0px"}}
+          onClick={ () => attackModelObject(requestDataObject) }>Reduce Input
+        </button>
+        </AccordionItemBody></AccordionItem></div>
+    )
+  }
+}
